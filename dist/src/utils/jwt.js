2ac7a4b06ffbbe5a12686d02b371abf2b44381f1
KevinChiguano <kevin_chili1998@hotmail.com>
2026-03-03 17:07:04 -0500

    chore: remove dist folder from git tracking and add to .gitignore

--- dist/src/utils/jwt.js
+++
@@ -1,6 +0,0 @@
-import jwt from "jsonwebtoken";
-import { env } from "../config/env";
-export const signToken = (payload) => jwt.sign(payload, env.JWT_SECRET, {
-    expiresIn: env.JWT_EXPIRES_IN,
-});
-export const verifyToken = (token) => jwt.verify(token, env.JWT_SECRET);
--- dist/src/utils/response.js
+++
@@ -1,9 +0,0 @@
-import { normalizeBigInt } from "./normalizeBigInt";
-export const ok = (data) => ({
-    success: true,
-    data: normalizeBigInt(data),
-});
-export const fail = (error) => ({
-    success: false,
-    error,
-});
